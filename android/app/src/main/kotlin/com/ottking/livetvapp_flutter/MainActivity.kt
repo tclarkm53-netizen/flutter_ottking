9717d51f6f7c6b9a27df921f63a496b20c0bdcb9
tclarkm53-netizen <tclarkm53@gmail.com>
2026-05-23 15:56:04 +0600

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/ottking/livetvapp_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/ottking/livetvapp_flutter/MainActivity.kt
@@ -1,5 +1,32 @@
 package com.ottking.livetvapp_flutter
 
-import io.flutter.embedding.android.FlutterActivity
+// আপনার আসল প্যাকেজ নাম দিন
 
-class MainActivity: FlutterActivity()
+import android.app.UiModeManager
+import android.content.Context
+import android.content.res.Configuration
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "app/platform_info"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "isAndroidTV") {
+                val isTV = checkIsAndroidTV()
+                result.success(isTV)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun checkIsAndroidTV(): Boolean {
+        val uiModeManager = getSystemService(Context.UI_MODE_SERVICE) as UiModeManager
+        return uiModeManager.currentModeType == Configuration.UI_MODE_TYPE_TELEVISION
+    }
+}
