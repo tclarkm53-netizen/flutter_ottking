f8d6696d482b20fc824e46704cd35e335b3e2783
tclarkm53-netizen <tclarkm53@gmail.com>
2026-05-23 17:31:22 +0600

    Update secure_interceptor.dart

--- lib/core/network/secure_interceptor.dart
+++ lib/core/network/secure_interceptor.dart
@@ -1,16 +1,9 @@
-import 'dart:math';
-import 'package:dio/dio.dart';
-import 'package:livetvapp_flutter/core/security/hmac_signer.dart';
-// আপনার AesCrypto ফাইলের সঠিক পাথ অনুযায়ী এটি ইম্পোর্ট করুন
-// import 'package:livetvapp_flutter/core/security/aes_crypto.dart'; 
-
+// lib/core/network/secure_interceptor.dart
 class SecureInterceptor extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
     final timestamp = DateTime.now().millisecondsSinceEpoch.toString();
-    final nonce = _generateNonce(); // random 8-char string
-    
-    // HMAC সিগনেচার তৈরি
+    final nonce = _generateNonce();  // random 8-char string
     final signature = HmacSigner.generateSignature(
       method: options.method,
       endpoint: options.path,
@@ -24,33 +17,17 @@ class SecureInterceptor extends Interceptor {
       'X-Nonce': nonce,
       'X-Signature': signature,
     });
-    
     handler.next(options);
   }
 
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
-    // সার্ভার থেকে আসা encrypted payload সেফলি ডিক্রিপ্ট করা
-    try {
-      if (response.data != null && response.data is Map && response.data['data'] != null) {
-        final encryptedBody = response.data['data'];
-        
-        if (encryptedBody is String) {
-          // ডেটা ডিক্রিপ্ট করে আবার রেসপন্সে অ্যাসাইন করা
-          // response.data['data'] = AesCrypto.decrypt(encryptedBody);
-        }
-      }
-    } catch (e) {
-      // ডিক্রিপশনে কোনো এরর হলে অ্যাপ ক্র্যাশ না করে এরর লগ করবে
-      print("Decryption Error: $e");
-    }
-    
+    // সার্ভার থেকে আসা encrypted payload মেমরিতে decrypt করো
+    final encryptedBody = response.data['data'] as String;
+    response.data['data'] = AesCrypto.decrypt(encryptedBody);
     handler.next(response);
   }
 
-  // নিরাপদ ও র্যান্ডম ৮ অক্ষরের নন্স (Nonce) জেনারেটর
-  String _generateNonce() {
-    final random = Random();
-    return List.generate(8, (_) => random.nextInt(36).toRadixString(36)).join();
-  }
+  String _generateNonce() =>
+    List.generate(8, (_) => Random().nextInt(36).toRadixString(36)).join();
 }
